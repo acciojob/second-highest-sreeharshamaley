d15a9653745b6e4f763c52253dba71d58cc9d2cf
Sree Harsha <102215646+sreeharshamaley@users.noreply.github.com>
2025-06-06 10:24:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+const getLargestNumber = (arr = []) => {
+let largestNumber = arr[0];
+let secondLargestNumber = arr[0];
+for (let i = 0; i < arr.length; i++) {
+if (arr[i] > largestNumber) {
+secondLargestNumber = largestNumber;
+largestNumber = arr[i];
+} else if (arr[i] > secondLargestNumber) {
+secondLargestNumber = arr[i] I
+}
+}
+return largestNumber;
+}
