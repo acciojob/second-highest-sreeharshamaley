d5dd27cf485afbbee63cea8cdba1260268a8e106
Sree Harsha <102215646+sreeharshamaley@users.noreply.github.com>
2025-06-06 10:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-//your JS code here. If required.
-const getLargestNumber = (arr = []) => {
-let largestNumber = arr[0];
-let secondLargestNumber = arr[0];
-for (let i = 0; i < arr.length; i++) {
-if (arr[i] > largestNumber) {
-secondLargestNumber = largestNumber;
-largestNumber = arr[i];
-} else if (arr[i] > secondLargestNumber) {
-secondLargestNumber = arr[i] I
-}
-}
-return largestNumber;
+const getSecondLargestNumber = (arr = []) => {
+    if (arr.length < 2) return -Infinity;
+
+    let largestNumber = -Infinity;
+    let secondLargestNumber = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largestNumber) {
+            secondLargestNumber = largestNumber;
+            largestNumber = arr[i];
+        } else if (arr[i] > secondLargestNumber && arr[i] !== largestNumber) {
+            secondLargestNumber = arr[i];
+        }
+    }
+
+    return secondLargestNumber === -Infinity ? -Infinity : secondLargestNumber;
 }
